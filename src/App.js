78a81a5f2e1e80e71ebb0160eaa3ea2ec59c8380
isabelxklee <hello@isabelklee.co>
2020-12-19 01:49:25 -0500

    created a Header and Wrapper component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from 'react'
+import Header from './components/Header.jsx'
+import Wrapper from './components/Wrapper.jsx'
 import './App.css'
 
-function App() {
-  return <div className="App"></div>
-}
+const App = () => (
+  <Wrapper>
+    <Header />
+    <section>
+      <h1>If you’ve always wanted to hop into my brain, today’s your lucky day.</h1>
+      <h3>Sometimes I will talk about heavy topics. Here are some example trigger warnings for my posts. If I missed any specific warnings, please let me know and I will add them. Thank you. ❤️</h3>
+    </section>
+  </Wrapper>
+)
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => (
+  <header>
+    <p className="left">isabel k. lee's diary</p>
+    <p className="right">proceed with caution</p>
+  </header>
+)
+
+export default Header
